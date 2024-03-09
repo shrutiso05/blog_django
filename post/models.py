9c4fb37b3850d08c6493d6e957913d224bbedb24
shrutiso05 <shristisoni051199@gmail.com>
2024-03-09 21:31:38 +0530

    setup_models_views

--- post/models.py
+++ post/models.py
@@ -2,11 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class BlogPost(models.Model):
+class Post(models.Model):
     title =  models.CharField(max_length=20)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     publication_date = models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete= models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    publicatation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+    
--- post/views.py
+++ post/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-from .models import BlogPost
+from django.shortcuts import render , redirect, get_object_or_404
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'Blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_deatil', pk=pk)
+        else:
+            form = CommentForm()
+        return render(request, 'blog/post_detail.html',{'post':post, 'comments' : comments, 'form' : form})
 # Create your views here.
